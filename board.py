0bed1aff3be156be15b32fcf7f6e410033e1e3e3
Joseph Nater <jnater@my.waketech.edu>
2022-04-18 22:53:49 -0400

    construct some of the game selection logic (patterns are still missing), player logic, and board logic

--- board.py
+++ board.py
@@ -1,23 +1,11 @@
-# Tic Tac Toe Rough Template
-# Stage 1 Submission - Joseph Nater (@jnater)
+# Tic Tac Toe Rough Draft Template 2
+# Joseph Nater (@jnater)
 # for CSC_120 Group 7
 
-# Because lists are mutable, we can easily manipulate selections
-# and treat it like a game.
+game_started = [False]
 
-tictactoe = [
-    '-','-','-',
-    '-','-','-',
-    '-','-','-'
-]
-
-# Create a default mapping of a clean board where 0-9 are
-# set to False when the game starts.
-
-tictactoe_assigned = [False for _ in tictactoe]
-
-player_1, player_1_name = "X", "Player 1"
-player_2, player_2_name = "O", "Player 2"
+player_1 = ["X", "Player 1"]
+player_2 = ["O", "Player 2"]
 
 # When a player marks a position, we assign it to the
 # appropriate list so we know which players have
@@ -26,24 +14,97 @@ player_2, player_2_name = "O", "Player 2"
 # actual game logic runs, these lists will determine
 # which player has which position.
 
-p1_assigned = []
-p2_assigned = []
+
+def setup_board():
+    # Because lists are mutable, we can easily manipulate selections
+    # and treat it like a game.
+
+    board = [
+        '-','-','-',
+        '-','-','-',
+        '-','-','-'
+    ]
+
+    # Create a default mapping of a clean board where 0-9 are
+    # set to False when the game starts.
+
+    board_assigned = [False for _ in board]
+    return board, board_assigned
 
 
-def print_board():
+def print_board(n):
     """
     To make reading the code easier, all the values of
     the tictactoe list are assigned to n
     from left to right, the positions are set 0-8, where 0
     is the 1st position
     """
-    n = tictactoe
     board = f"""
         {n[0]}|{n[1]}|{n[2]}
         {n[3]}|{n[4]}|{n[5]}
         {n[6]}|{n[7]}|{n[8]}
     """
-    return board
+    print(board)
+
+
+def loop_game():
+    # Setup a clean board to work with
+    board, board_assigned = setup_board()
+
+
+    # Start the actual game logic
+    while True:
+
+        # Cycle between player 1, and player 2
+        for p in [player_1, player_2]:
+
+            # Print the actual board
+            print_board(board)
+            # Check for any winning patterns
+            for char in ["O", "X"]:
+                pattern = (char, char, char)
+                # Capture "XXX" or "OOO" pattern
+                # TODO: Add all different winning patterns (possible options of winning)
+                # e.g check for each row, and also check diagonally |X|
+                #                                                     |X|
+                #                                                       |X|
+                if pattern == (board[0], board[1], board[2]):
+                    if char == "O":
+                        print("Player 2 wins!")
+                    if char == "X":
+                        print("Player 1 wins!")
+                    return
+            
+            retry = True
+            # Wait for an actual choice from the player
+            while retry:
+                try:
+                    choice = int(input(f"{p[1]}, make a selection (1-9): "))-1
+                except ValueError:
+                    print("Please make a valid selection from 1-9.")
+                    continue
+                except KeyboardInterrupt:
+                    game_started[0] = False
+                    print()
+                    break
+                if choice+1 not in range(1,10):
+                    print("Please make a valid selection from 0-9.")
+                    continue
+                if board_assigned[choice] != False:
+                    print("That choice has already been selected.")
+                    print_board(board)
+                    # Retry to get the player's choice if it is invalid
+                    retry = True
+                else:
+                    board_assigned[choice] = True
+                    board[choice] = p[0]
+                    retry = False
+            if not game_started[0]:
+                break
+        if not game_started[0]:
+            break
+
+
 
 
 def main():
@@ -52,7 +113,12 @@ def main():
     # and each time we request an input from player 1 or 2.
     # Game logic can either start randomly (player 2 starts first)
     # (or player 1 starts first)
-    print(print_board())
+
+    game_started[0] = True
+
+    while game_started[0]:
+        loop_game()
+        game_started[0] = input("Would you like to play again? (y/n): ") == 'y' and True or False
 
 
 main()
